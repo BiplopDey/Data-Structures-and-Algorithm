f128c041b03c61ac7871dd1314c0c3a1b714d6f4
BiplopDey <52823579+BiplopDey@users.noreply.github.com>
2023-07-24 00:58:23 +0200

    anagram refactored

--- src/test/java/com/datastructuresandalgorithm/datastructuresandalgorithm/pruebas/MinTransaction.java
+++ src/test/java/com/datastructuresandalgorithm/datastructuresandalgorithm/pruebas/MinTransaction.java
@@ -3,7 +3,6 @@ package com.datastructuresandalgorithm.datastructuresandalgorithm.pruebas;
 import org.junit.jupiter.api.Test;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
 import static java.lang.Math.abs;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -12,120 +11,95 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 public class MinTransaction {
     class Balance {
         private final Map<String, Double> balance = new HashMap<>();
-        public Balance(){
+
+        public Balance() {
         }
 
         public void addTransaction(String payer, List<String> participants, Double amount) {
-            Double eachShare = amount / participants.size();
+            var eachShare = amount / participants.size();
 
             addIfNewParticipant(participants);
 
-            participants.forEach(p->{
-                balance.put(p, balance.get(p)+eachShare);
-            });
-            balance.put(payer, balance.get(payer)-amount);
+            participants.forEach(p -> balance.put(p, balance.get(p) + eachShare));
+            balance.put(payer, balance.get(payer) - amount);
         }
 
         public Double balanceOf(String name) {
-            if(!balance.containsKey(name))
+            if (!balance.containsKey(name))
                 throw new IllegalArgumentException();
             return balance.get(name);
         }
 
         private void addIfNewParticipant(List<String> participants) {
-            participants.forEach(p->{
-                if(!balance.containsKey(p)) balance.put(p, (double) 0);
-            });
+            participants.stream().filter(p -> !balance.containsKey(p)).forEach(p -> balance.put(p, 0.0));
         }
 
         public List<Transaction> getTransactions() {
-            List<Transaction> transactions = new ArrayList<>();
-            Map<String, Double> map = new HashMap<>(balance);
+            var transactions = new ArrayList<Transaction>();
+            var map = new HashMap<>(balance);
             //[-350,..,20]
-            //[-5,...,20]
-            while (!areAllBalanceZero(map)){
-                String maxPerson = getMax(map);
-                String minPerson = getMin(map);
-                Double min = map.get(minPerson), max = map.get(maxPerson);
-                if(abs(min)>=max){
-                    map.put(maxPerson, (double) 0);
-                    map.put(minPerson, min+max);
-                    transactions.add(new Transaction(maxPerson, minPerson, max));
-                }
-                if(abs(min)<max){
-                    map.put(maxPerson, min+max);
-                    map.put(minPerson, (double) 0);
-                    transactions.add(new Transaction(maxPerson, minPerson, abs(min)));
-                }
+            //[-5,..,20]
+            while (!areAllBalanceZero(map)) {
+                var max = getMax(map);
+                var min = getMin(map);
+                var transfer = Math.min(abs(min.getValue()), max.getValue());
+                map.put(max.getKey(), max.getValue() - transfer);
+                map.put(min.getKey(), min.getValue() + transfer);
+                transactions.add(new Transaction(max.getKey(), min.getKey(), transfer));
             }
-            
+
             return transactions;
         }
 
         private boolean areAllBalanceZero(Map<String, Double> map) {
-            for(Map.Entry<String, Double> e : map.entrySet()){
-                if(abs(e.getValue()) > Double.MIN_VALUE)
+            for (Map.Entry<String, Double> e : map.entrySet()) {
+                if (abs(e.getValue()) > Double.MIN_VALUE)
                     return false;
             }
             return true;
         }
 
-        private String getMin(Map<String, Double> map) {
-            return map.entrySet().stream()
-                    .min(Comparator.comparing(Map.Entry::getValue))
-                    .get()
-                    .getKey();
+        private Map.Entry<String, Double> getMin(Map<String, Double> map) {
+            return Collections.min(map.entrySet(), (a,b)->  a.getValue().compareTo(b.getValue()));
         }
 
-        private String getMax(Map<String, Double> map) {
-            return map.entrySet().stream()
-                    .max(Comparator.comparing(Map.Entry::getValue))
-                    .get()
-                    .getKey();
+        private Map.Entry<String, Double> getMax(Map<String, Double> map) {
+            return Collections.max(map.entrySet(), (a,b)->  a.getValue().compareTo(b.getValue()));
         }
     }
 
-    class Transaction{
-        final String from;
-        final String to;
-        final Double amount;
-
-        public Transaction(String from, String to, Double amount){
-            this.from = from;
-            this.to = to;
-            this.amount = amount;
-        }
+    record Transaction(String from, String to, Double amount) {
 
         @Override
-        public String toString(){
-            return "From "+from+" To " + to + " Amount "+ amount;
+        public String toString() {
+            return String.format("From %s to %s amount %s ", from, to, amount);
         }
     }
 
     @Test
-    public void testGetBalanceOf(){
+    public void testGetBalanceOf() {
         Balance balance = new Balance();
         // ana -> buy to every one 400
         // jon -> buy 100 to bob and mar
-        balance.addTransaction("Ana", List.of("Ana","Jon","Bob","Mar"), Double.valueOf(400));
-        balance.addTransaction("Jon", List.of("Bob","Mar"), Double.valueOf(100));
+        balance.addTransaction("Ana", List.of("Ana", "Jon", "Bob", "Mar"), 400.0);
+        balance.addTransaction("Jon", List.of("Bob", "Mar"), 100.0);
 
-        assertEquals(Double.valueOf(0), balance.balanceOf("Jon"));
-        assertEquals(Double.valueOf(150), balance.balanceOf("Bob"));
-        assertEquals(Double.valueOf(150), balance.balanceOf("Mar"));
-        assertEquals(Double.valueOf(-300), balance.balanceOf("Ana"));
-        assertThrows(IllegalArgumentException.class, ()->balance.balanceOf("Dave"));
+        assertEquals(0.0, balance.balanceOf("Jon"));
+        assertEquals(150.0, balance.balanceOf("Bob"));
+        assertEquals(150.0, balance.balanceOf("Mar"));
+        assertEquals(-300.0, balance.balanceOf("Ana"));
+        assertThrows(IllegalArgumentException.class, () -> balance.balanceOf("Dave"));
     }
 
     @Test
-    public void testMinimumTransaction(){
+    public void testMinimumTransaction() {
         Balance balance = new Balance();
 
-        balance.addTransaction("Ana", List.of("Ana","Jon","Bob","Mar"), Double.valueOf(400));
-        balance.addTransaction("Jon", List.of("Bob","Mar"), Double.valueOf(150));
+        balance.addTransaction("Ana", List.of("Ana", "Jon", "Bob", "Mar"), 400.0);
+        balance.addTransaction("Jon", List.of("Bob", "Mar"), 150.0);
 
-     //   assertEquals(2,balance.getTransactions().size());
-        balance.getTransactions().forEach(p->System.out.println(p));
+        //   assertEquals(2,balance.getTransactions().size());
+        balance.getTransactions().forEach(System.out::println);
     }
 
 }
